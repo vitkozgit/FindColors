212f3ec5b6b82d5bbf35d929648fb79937651e25
vitkozgit <vitaligit@gmail.com>
2017-04-09 00:45:49 +0200

    Vines nahuy kusok kon9!!!

--- main.cpp
+++ main.cpp
@@ -6,17 +6,6 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     FindColors w;
     w.show();
-////////////////////////////////////////////////////////////////
-    cv::Vec3b vec(0,2,123);
-    //std::cout << static_cast<int>(vec[0]) << std::endl;
-    std::cout << (int)vec[2] << std::endl;
-    int x = vec[0];
-    std::cout << x << std::endl;
-
-    uchar xx = 3, yy = 5, zz;
-    zz = xx * xx + yy * yy;
-    std::cout << (int)zz << std::endl;
-/////////////////////////////////////////////////////////////////
 
     return a.exec();
 }
